03f1423e1507c020c98420e1513302ea4a631055
Stephen Tueller <stueller@rti.org>
2019-06-13 15:06:14 -0600

    move myPolyICT explicitly to test_ICTpower

--- tests/testthat/test_ICTpower.R
+++ tests/testthat/test_ICTpower.R
@@ -3,7 +3,19 @@ library(PersonAlyticsPower)
 
 test_that("piecewise",
 {
-  example(polyICT)
+  myPolyICT <- polyICT$new(
+    groups            = c(group1=10, group2=10)          ,
+    phases            = makePhase()                      ,
+    propErrVar        = c(randFx=.5, res=.25, mserr=.25) ,
+    randFxOrder       = 1                                ,
+    randFxCor         = 0.2                              ,
+    randFxVar         = c(1, 1)                          ,
+    error             = armaErr$new()                    ,
+    merror            = armaErr$new(list())              ,
+    ySD               = 15                               ,
+    yMean             = 100                              ,
+  )
+
   myPolyICTnonPar <- myPolyICT$clone(deep=TRUE)
   myPolyICTnonPar$update(groups=c(group1=500, group2=500))
   Data <- myPolyICTnonPar$makeData()
@@ -16,7 +28,7 @@ test_that("piecewise",
            sampleSizes  = c(25,25)             ,
            alignPhase   = 'piecewise'          ,
            prompt       = FALSE                ,
-           debugforeach = TRUE                 )
+           debugforeach = FALSE                )
 
 
 
